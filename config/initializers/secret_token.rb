4dcca2862e42e34e0028fd137fd9a0780a4e7ca3
Benjamin Cullen-Kerney <ben@beancuke.com>
2012-04-02 20:21:39 -0700

    rails new hstore_blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HstoreBlog::Application.config.secret_token = '420437999eae2d64778938106cd72fd1b3269a6990b85e825eca0086bb0c6f8a86a17c858eadcdac98c426f861d5905a767fbf69e8e1b66a43df515f371a85c2'
